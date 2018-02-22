b4d2784e55486249d21306c58ba43f6764f218ac
Anwesh43 <anweshthecool0@gmail.com>
2018-02-22 08:41:33 +0530

    using view in activity

--- app/src/main/java/ui/anwesome/com/kotlincirclelinemoverview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlincirclelinemoverview/MainActivity.kt
@@ -2,11 +2,12 @@ package ui.anwesome.com.kotlincirclelinemoverview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ui.anwesome.com.circlelinemoverview.CircleLineMoverView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        CircleLineMoverView.create(this)
     }
 }
