9a8ffbb3f92cb51085515b84d08d91ac831a8605
Anwesh43 <anweshthecool0@gmail.com>
2018-02-22 07:37:32 +0530

    adding CircleLineMoverView class

--- circlelinemoverview/src/main/java/ui/anwesome/com/circlelinemoverview/CircleLineMoverView.kt
+++ circlelinemoverview/src/main/java/ui/anwesome/com/circlelinemoverview/CircleLineMoverView.kt
@@ -3,3 +3,20 @@ package ui.anwesome.com.circlelinemoverview
 /**
  * Created by anweshmishra on 22/02/18.
  */
+import android.content.*
+import android.view.*
+import android.graphics.*
+class CircleLineMoverView(ctx:Context):View(ctx) {
+    val paint:Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    override fun onDraw(canvas:Canvas) {
+
+    }
+    override fun onTouchEvent(event:MotionEvent):Boolean {
+        when(event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
